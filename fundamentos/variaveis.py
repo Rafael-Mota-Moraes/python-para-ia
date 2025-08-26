625346eec59f223bac9673918bac39cd92f849ee
Rafael Mota Moraes <rafaelmota622@gmail.com>
2025-08-26 19:37:17 -0300

    Variáveis, tipos e entradas do usuário

---
+++ fundamentos/variaveis.py
@@ -0,0 +1,21 @@
+nome = "Rafael"
+idade = 19
+altura = 1.65
+mensagem = "Bom dia!"
+
+print(nome)
+print(idade)
+print(altura)
+print(mensagem)
+
+# Tipos
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(mensagem))
+
+print("\n\n\nInteração com o usuário\n\n\n")
+
+novo_nome = input("Digite seu nome: ")
+print("Nome digitado pelo usuário", novo_nome)
