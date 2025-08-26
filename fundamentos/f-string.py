5bb8786a81d483bdbc6ca7c77c4fbf6f777af21f
Rafael Mota Moraes <rafaelmota622@gmail.com>
2025-08-26 19:44:30 -0300

    Melhorando o print com f-string

---
+++ fundamentos/f-string.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print(f"Olá, {nome}, você tem {idade} anos de idade")
+# print(f"Olá, {nome}, daqui 5 anos você terá {idade + 5} anos de idade")
+# Como resolver?
