f44525aa283f73148afed5d370f6788eec47f065
Rafael Mota Moraes <rafaelmota622@gmail.com>
2025-08-30 17:52:21 -0300

    Operadores aritimeticos e desafios do dia a dia

---
+++ fundamentos/matematica.py
@@ -0,0 +1,23 @@
+# nome = input("Digite seu nome: ")
+# idade = int(input("Digite sua idade: "))
+
+# print(f"Olá, {nome}, daqui 5 anos você terá {idade + 5} anos de idade")
+
+# print(type(nome))
+# print(type(idade))
+
+num1 = 10
+num2 = 2
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+preco = 50  # valor em R$
+desconto = 10  # desconto em %
+novo_preco = preco - (preco * desconto / 100)
+print(f'O valor com desconto é de: R$ {novo_preco}')
