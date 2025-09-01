7714900ec01187b49326cd2ad32b1916eb6d36c3
Rafael Mota Moraes <rafaelmota622@gmail.com>
2025-09-01 13:08:10 -0300

    Funções

---
+++ fundamentos/funcoes.py
@@ -0,0 +1,12 @@
+def saudacao(nome):
+    print(f'Olá, {nome}')
+
+saudacao('Rafael')
+saudacao('João')
+saudacao('Manoel')
+
+def somar(num1, num2):
+    return num1 + num2
+
+soma = somar(1, 2)
+print(soma)
